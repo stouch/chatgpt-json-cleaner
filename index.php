36db9dcbdf34b46cb8be18c7197ec1c2bb352a80
stouch <syl20.touchard@gmail.com>
2023-03-05 12:24:31 +0100

    Update index.php

--- index.php
+++ index.php
@@ -7,20 +7,28 @@ function cleanJSON($json, $missingKeyName = 'details')
         
         $json = str_replace(['“', '”', '`'], '"', $json);
         $json = str_replace([':-)', ' :D ', ' :P ', ' :p '], '', $json); // We may find emojis in comments that gonna create ambiguities with properties semicol...
+        $json = preg_replace('/\/\/([^"\/]+)[\s]{5,}/', '', $json); // Clean obvious comments  `  // Lorem ipsum         ` 
         $json = preg_replace('/\/\/([\s]*Ex(a|e)*(mple)*)[\s]*:/', '// Ambigous comment', $json); // Pre-clean ambigous comments  `  // Example: ... ` for which we could not say if "Example" is a property
+        $steps[0] = $json;
 
         $json = preg_replace('/""([\wA-zÀ-ú])/', '"$1', $json); // Clean : ` ""Lorem ipsum dolor sit amet.. `
         $json = preg_replace('/[\s+]\.[\s]*"/', ' , "', $json); // Clean  `  .  "location": ... ` When it outputs dot instead of comma
 
-        $json = preg_replace('/[\s]+(\w+)[\s]*:/', '"$1":', $json); // Clean:  `  location: ... 
+
         $json = preg_replace('/,[\s]+(\w+)"[\s]*:/', ',"$1":', $json); // Clean:  ` , ... location": ... 
         $json = preg_replace('/"[\s]*\+[\s]*"/', ' ', $json); // Clean: ` "  +  " ` in bullet points texts
         $json = preg_replace('/([\s]+)(\w+)[\s]*:([\wA-zÀ-ú\-\(\)’\'\ \,\.\;]+)\}/', '$1"$2": "$3"}', $json); // Clean: `  location : Paris  ... }   `
         $json = preg_replace('/\"(\w+)\"[\s]*:([\wA-zÀ-ú\-\(\)’\'\ \,\.\;]+)\}/', '"$1": "$2"}', $json); // Clean: `  "location" : Paris  ... }  `
-        $json = preg_replace('/([\s]+)(\w+)[\s]*:([\wA-zÀ-ú\-\(\)’\'\ \.\;]+)\,/', '$1"$2": "$3",', $json); // Clean: `  location : Paris  ... ,   `
+        $json = preg_replace('/([\s]+)(\w+)[\s]*:([\wA-zÀ-ú\-\(\)’\'\ \.\;]+)\,[\s+]*"/', '$1"$2": "$3", "', $json); // Clean: `  location : Paris  ... ,  "another"...`
         $json = preg_replace('/\"(\w+)\"[\s]*:([\wA-zÀ-ú\-\(\)’\'\ \.\;]+)\,/', '"$1": "$2",', $json); // Clean: `  "location" : Paris  ... ,  `
         $json = preg_replace('/([\s]{0,1})"(\w+)"[\s]*:([\wA-zÀ-ú\-\(\)’\'\ \.\;]+)\"[\s]*(,|})/', '$1"$2": "$3" $4', $json); // Clean: `  "location" : Paris ..."  `
-        $json = preg_replace('/([\s]{0,1})(\w+)[\s]*:([\wA-zÀ-ú\-\(\)’\'\ \.\;]+)\"[\s]*(,|})/', '$1"$2": "$3" $4', $json); // Clean: `  location : Paris ..."  `
+
+        $hasBadStartedKeys = 1;
+        while ($hasBadStartedKeys > 0) {
+            $json = preg_replace('/([\s]{2,}|\"[\s]*,[\s]+)(\w+)[\s]*:[\s]*\"([^"]+)\"[\s]*(,|})/', '$1"$2": "$3"$4', $json, -1, $hasBadStartedKeys); // Clean: `  location: "Paris ..."  `
+        }
+
+        $json = preg_replace('/([\s]{2,})(\w+)[\s]*:([\wA-zÀ-ú\-\(\)’\'\ \.\;]+)\"[\s]*(,|})/', '$1"$2": "$3"$4', $json); // Clean: `  location : Paris ..."  ` but MUST not be inside a text value (with 2 space before, we conclude a chatgpt mistake)
 
         $hasUnfinishedValues = 1;
         while ($hasUnfinishedValues > 0) {
@@ -38,12 +46,14 @@ function cleanJSON($json, $missingKeyName = 'details')
         }
         $json = preg_replace('/([^":\s]+)"[\s]*,[^"]{2,}\}/', '$1"}', $json); // Clean any garbage in the json end before }
         $json = preg_replace('/"[\s]*,[\s]*"[\s]*,[\s]*"/', '", "', $json); // Clean lost quotes
+        $json = preg_replace('/"[\s]*,[\s]*"[\s]*}/', '"}', $json); // Clean lost quotes at the end
         $json = preg_replace('/\}[\s]*,[\s]*\]/', '}]', $json); // Clean misc garbage
 
         $json = preg_replace('/",[\s]+-/', '", "' . $missingKeyName . '": "', $json); // Ugly fix to handle bug ` ", .... - lorem ipsum", ... ` (missing json key before the value when tirets)
-        
-        $json = trim($json, ".\n\r\t\v\x00");
-        
+
+        $json = rtrim($json, '.');
+        $json = trim($json, "\n\r\t\v\x00");
+
         return $json;
 
 }
